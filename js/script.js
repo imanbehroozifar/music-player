ae520d9a9875866f161034b95dd550903a038b09
Iman Behroozifar <105700046+imanbehroozifar@users.noreply.github.com>
2023-07-24 08:20:44 +0430

    Development of the ui part of the project

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Music
+
+const songs = [
+    {
+        path: 'media/SirvanKhosravi-Emrooz Mikham.mp3',
+        displayName: 'Emrooz Mikham behet begam',
+        artist: 'Sirvan Khosravi',
+        cover:'images/sirvan-khosravi.jpg'
+    },
+    {
+        path: 'media/Ali Yasini - Alaki (128).mp3',
+        displayName: 'Alaki',
+        artist: 'Ali Yasini',
+        cover:'images/yasini.jpg'
+    },
+    {
+        path: 'media/Ehsan Khajeh Amiri - Yekio Daram (128).mp3',
+        displayName: 'Yekio Daram',
+        artist: 'Ehsan Khajeh Amiri',
+        cover:'images/ehsan.jpg'
+    },
+    {
+        path: 'media/Garsha Rezaei - Mage Paeez Oomade 128.mp3',
+        displayName: 'Mage Paeez Oomade',
+        artist: 'Garsh Razaei',
+        cover:'images/rezaii.jpg'
+    },
+    {
+        path: 'media/Mehdi Ahmadvand - Mashine Zaman [128].mp3',
+        displayName: 'Mashine Zaman',
+        artist: 'Mehdi Ahmadvand',
+        cover:'images/Mehdi-Ahmadvand.jpg'
+    }
+]
